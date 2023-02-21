4633f0b0939c41aa12bc89d624266e173db2e562
bykriz <cristianesquivelh@hotmail.com>
2023-02-21 03:02:32 -0400

    configurando tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'white': '#fff'
+    },
+    extend: {
+      boxShadow: {
+        'gray-400': '#9ca3af',
+      }
+    },
   },
   plugins: [],
 };
