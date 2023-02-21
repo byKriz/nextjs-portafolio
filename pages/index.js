f3b29320af9ec3e757addbe5b17c968807f45140
bykriz <cristianesquivelh@hotmail.com>
2023-02-21 03:23:30 -0400

    implementando el navbar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
+import { Navbar } from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className="text-3xl underline">Hello world!</h1>
+        {/* <h1 className="text-3xl underline">Hello world!</h1> */}
+        <Navbar />
       </main>
     </>
   );
